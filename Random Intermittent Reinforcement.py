a29e02047a5369d11127532732c92e02c1a9bfe0
Bryan <101913689+B-ryn@users.noreply.github.com>
2022-10-19 16:24:50 -0500

    Update Random Intermittent Reinforcement.py

--- Random Intermittent Reinforcement.py
+++ Random Intermittent Reinforcement.py
@@ -1,11 +1,21 @@
+"""
+A neuroscientist by the name of Andrew Huberman declared that the best way to reinforce a behavior is to reward an action 1/3rd of the time. 
+This is called random intermittent reinforcement and it is one of the most effective ways to reinforce a behavior. 
+Even casinos know this as they let the user win 1/3rd of the time with their gambles.
+
+This program is designed to mimicate that random intermittent reinforcement so that the user can use it to their advantage. 
+"""
+
+# Importing libraries
 import random
 from fractions import Fraction
 
-
+# Declaring the varibles
 num_input = None
 
 reward_num = None
 
+# The paragraph indicating the title and instructions
 greeting = 'Welcome to the Dopamine Reward Program!!!\n\n' \
            'Please type in the letter correlating to the diffculity that you wish to recieve your reward by!\n\n' \
            'A) Easy\n' \
@@ -14,6 +24,8 @@ greeting = 'Welcome to the Dopamine Reward Program!!!\n\n' \
 
 print(greeting)
 
+
+# The varibles that determine the difficulty of the reward
 easy = random.randrange(1, 6)
 
 medium = random.randrange(1, 15)
@@ -21,6 +33,7 @@ medium = random.randrange(1, 15)
 hard = random.randrange(1, 21)
 
 
+# While loop that verifys if the user is implementing the right choices
 while True:
     num_input = input('Your choice: ')
     num_input = num_input.upper()
@@ -34,12 +47,8 @@ while True:
         continue
 
 
-
-print(num_input)
-print('pre if: ' , str(easy) , str(medium) , str(hard))
-
-
-# Maybe put this in a function to make it look cleaner?
+# If statement that figures the input and correlates the input to the difficulty and creates the reward
+# by multiplying the max number with 1/3rd
 if num_input == 'A':
     num_input = easy
     reward_num = Fraction(1, 3) * 6
@@ -51,11 +60,12 @@ elif num_input == 'C':
     reward_num = Fraction(1, 3) * 21
 
 
+# The end statment where it declares if you got the reward or not and indicates what you need and what you recieved
 if num_input <= reward_num:
     print("Reward!")
     print("Needed under " + str(reward_num))
-    print("Got " + str(num_input))
+    print("You got " + str(num_input))
 else:
     print('No Reward!')
     print("Needed equal or under " + str(reward_num))
-    print("Got " + str(num_input))
+    print("You got " + str(num_input))
